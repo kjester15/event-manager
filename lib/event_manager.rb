6bbae544abd54943fef38c4278dc4d15c666d0b7
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-07-03 15:48:39 -0700

    Complete 'Clean Phone Numbers' portion of assignment

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,27 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(number)
+  # remove any non-numbers, including spaces, from the number
+  number.gsub!(/[^0-9]/, '')
+
+  # get length of string
+  num_length = number.length
+
+  # determine if number is good or bad
+  if num_length < 10
+    'Invalid Number'
+  elsif num_length == 10
+    number
+  elsif num_length == 11 && number[0] == '1'
+    number[1..10]
+  elsif num_length == 11 && number[0] != '1'
+    'Invalid Number'
+  else
+    'Invalid Number'
+  end
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -47,6 +68,8 @@ contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
+
+  phone_number = clean_phone_number(row[:homephone])
 
   legislators = legislators_by_zipcode(zipcode)
 
